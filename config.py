c23801422e23b63f509c15f0f2168aed0ac85f67
Alexander Konrad Kessler <skaownz234@gmail.com>
2020-01-18 14:56:31 -0600

    init

---
+++ config.py
@@ -0,0 +1,6 @@
+dbname = 'testdb'
+user = 'testuser'
+password = 'sasdfsda'
+host = 'localhost'
+
+CONNECTION_STRING = f"dbname='{dbname}' user='{user}' password='{password}' host='{host}'"
---
+++ scrape_links.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup as BS
+from time import time
+import psycopg2
+import requests
+import config
+import sys
+import re
+
+init_ts = time()
+
+links = list(filter(lambda x: bool(x), [
+    a.get('href', False) for a in 
+    BS(requests.get(f'https://{sys.argv[1]}').text).find_all('a')
+]))
+
+with open('links.txt', 'w+') as fh:
+    fh.write('\n'.join(links))
+
+connection = psycopg2.connect(config.CONNECTION_STRING)
+cursor = connection.cursor()
+
+for link in links:
+    cursor.execute(
+        "INSERT INTO LINKS(HREF, DOMAIN) VALUES(%s, %s)",
+        (link, sys.argv[1],)
+    )
+    print(f'INSERTED {link}')
+
+connection.commit()
+
+print(f'DONE AFTER {int(time()-init_ts)}s')
